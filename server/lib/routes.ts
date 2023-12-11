2ee3ef65cd86946cfe8984919204041a387198c2
Claudio Cicali <claudio.cicali@gmail.com>
2023-12-11 17:10:39 +0100

    Better typing, eslint and HTML fixes

--- server/lib/routes.ts
+++ server/lib/routes.ts
@@ -1,10 +1,25 @@
 import { Express, Request, Response } from 'express';
 import { cssFile, jsFile } from './assets';
 
+/* The following 3 interfaces are of course up to you to define and keep using */
+
+interface PartialModel {
+  currentTime: string;
+}
+
+interface IndexModel extends PartialModel {
+  cssFile: string;
+  jsFile: string;
+}
+
+interface AppResponse<T> extends Omit<Response, 'render'> {
+  render(view: string, options: T): void;
+}
+
 export default (app: Express) =>
   app
     // Renders the home page
-    .get('/', async (req: Request, res: Response) => {
+    .get('/', async (req: Request, res: AppResponse<IndexModel>) => {
       res.render('index', {
         cssFile,
         jsFile,
@@ -13,8 +28,11 @@ export default (app: Express) =>
     })
 
     // Renders the server time partial upon HTMX request
-    .get('/api/server-time', async (req: Request, res: Response) => {
-      res.render('partials/serverTime', {
-        currentTime: new Date().toISOString(),
-      });
-    });
+    .get(
+      '/api/server-time',
+      async (req: Request, res: AppResponse<PartialModel>) => {
+        res.render('partials/serverTime', {
+          currentTime: new Date().toISOString(),
+        });
+      }
+    );
