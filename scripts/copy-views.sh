0349808f65b09b6c017048f4bf3931d5748a4d87
Claudio Cicali <claudio.cicali@gmail.com>
2023-12-23 13:58:56 +0100

    Fix the copy views script

--- scripts/copy-views.sh
+++ scripts/copy-views.sh
@@ -2,5 +2,5 @@
 
 # Tiny helper to copy server views to dist folder when building
 # During dev, the server views are served from the server folder
-viewsPath="server/views"
-test -d ${viewsPath} && cp -r ./${viewsPath} ./dist/${viewsPath}
+viewsPath="server/views/"
+test -d ${viewsPath} && rsync -ah --delete ${viewsPath} dist/${viewsPath}
